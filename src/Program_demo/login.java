6d4565086afe3783efc4e30404baa6e7d4060286
Virus <Virus@DESKTOP-M0AEUJR>
2024-06-18 10:28:28 +0530

    {Vikas] : new generate project updated

---
+++ src/Program_demo/login.java
@@ -0,0 +1,22 @@
+package Program_demo;
+
+public class login {
+	
+	
+	
+
+	
+	public static void main(String[] args) {
+		
+	
+		System.out.println("new generate project");
+		
+		
+	}
+
+	
+	
+	
+	
+
+}
---
+++ src/Program_demo/logout.java
@@ -0,0 +1,12 @@
+package Program_demo;
+
+public class logout {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("second project");
+	}
+
+}
